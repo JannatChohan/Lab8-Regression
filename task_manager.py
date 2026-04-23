80b7f49fa3007fffbde4693fa9a428a9b1874bd4
JannatChohan <jannatchohanf23@nutech.edu.pk>
2026-04-23 10:58:54 +0500

    task_manager.py

--- task_manager.py
+++ task_manager.py
@@ -10,8 +10,10 @@ class TaskManager:
             self.tasks[index]["completed"] = True
 
     def get_completion_rate(self):
+        if len(self.tasks) == 0:
+            return 0
         completed = 0
         for t in self.tasks:
             if t["completed"]:
                 completed += 1
-        return completed / (len(self.tasks) + 1)
+        return completed / len(self.tasks)
